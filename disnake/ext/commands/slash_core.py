3a1c1f28f7d3de37e730322b8b51e9e829f37241
MagicTheDev <82341152+MagicTheDev@users.noreply.github.com>
2024-04-21 19:09:40 -0500

    Update slash_core.py
    Signed-off-by: MagicTheDev <82341152+MagicTheDev@users.noreply.github.com>

--- disnake/ext/commands/slash_core.py
+++ disnake/ext/commands/slash_core.py
@@ -87,7 +87,7 @@ async def _call_autocompleter(
         else:
             choices = autocomp(inter, user_input, **filled)
     except TypeError:
-        if requires_cog_param:
+        if not requires_cog_param:
             choices = autocomp(cog, inter, user_input)
         else:
             choices = autocomp(inter, user_input)
